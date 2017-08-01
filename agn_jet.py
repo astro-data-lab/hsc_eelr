21282509ca2fa6ab69e4d89abe7f4a524c694a22
Peter Melchior <peter.m.melchior@gmail.com>
2017-08-01 10:27:20 -0400

    initial commit

---
+++ agn_jet.py
@@ -0,0 +1,71 @@
+import numpy as np
+import proxmin
+import deblender
+
+# load data
+#import astropy
+import fitsio
+bands = ['g','r','i','z','y']
+objname = 'SDSSJ0838+0150'
+data_bands = []
+for b in bands:
+    hdu = fitsio.FITS("%s/stamp-%s.fits" % (objname, b))
+    data_bands.append(hdu[0][:,:])
+    hdu.close()
+data = np.array(data_bands)
+
+# need to get weights
+weights = None
+
+# load expected jet SED
+#jet_sed = np.zeros(len(bands))
+#specdata = np.loadtxt('%s/spec_mag.csv' % (objname))
+jet_sed = np.array([0.06598638046801182,0.2032376761774897,1.9325388133884376,0,1.1942058574708945])
+#jet_sed = proxmin.operators.prox_unity_plus(jet_sed, 1)
+
+# load peak position
+peaks = [[60,59], [63,48], [58,15], [26,38], [55,73]]
+constraints = ["cS"] * len(peaks)
+
+# add jet component
+peaks = peaks + [peaks[0]]
+constraints = constraints + [None]
+
+# restrict to inner 49 pixels
+shape = data[0].shape
+dx = (shape[0] - 49)/2
+dy = (shape[1] - 49)/2
+data = data[:,dx:-dx,dy:-dy]
+peaks = np.array(peaks) - np.array((dx,dy))
+inside = (peaks[:,0] > 0) & (peaks[:,1] > 0)
+peaks = peaks[inside]
+constraints = [constraints[i] for i in range(len(constraints)) if inside[i] == 1]
+
+# define constraints
+def prox_SED(A, step, jet_sed=None):
+    A[:,-1] = jet_sed
+    return proxmin.operators.prox_unity_plus(A, step, axis=0)
+
+from functools import partial
+prox_A = partial(prox_SED, jet_sed=jet_sed)
+
+# run deblender
+result = deblender.nmf.deblend(data,
+    peaks=peaks, weights=weights,
+    psf=None,
+    constraints=constraints,
+    prox_A=prox_A,
+    monotonicUseNearest=False,
+    max_iter=10,
+    e_rel=[1e-6,1e-3],
+    l0_thresh=5e-4,
+    traceback=False,
+    update_order=[1,0])
+A, S, model, P_, Tx, Ty, tr = result
+
+"""
+for k in range(len(S)):
+    component = deblender.nmf.get_peak_model(A[:,k], S[k].flatten(), Tx[k], Ty[k], shape=(S[k].shape))[0]
+    figure()
+    imshow(np.ma.array(component, mask=component==0))
+"""
---
+++ prepare_data.py
@@ -0,0 +1,82 @@
+import numpy as np
+import galsim
+import os
+
+def getDiffKernel(P, P0, nx=50, ny=50):
+    PD = galsim.Convolve(P, galsim.Deconvolve(P0))
+    PDimg = PD.drawImage(nx=nx,ny=ny, scale=pixel_scale, method='no_pixel')
+    return PDimg
+
+def getDiffKernels(psf_bands, P0, thresh=5e-3):
+    kernels = []
+    diff_kernels = []
+    reconv_kernels = []
+    psf_error = 0
+
+    for i in range(len(psf_bands)):
+        P = psf_bands[i]
+        nx,ny = P.image.array.shape
+
+        PDimg = getDiffKernel(P, P0, nx=nx, ny=ny)
+        kernels.append(P.image.array)
+        diff_kernels.append(PDimg.array)
+
+        # just for testing
+        P0img = P0.drawImage(nx=nx,ny=ny, scale=pixel_scale, method='no_pixel')
+        P0FromImage = galsim.InterpolatedImage(P0img)
+
+        # truncate difference kernel below 3e-3
+        # to make the more compact
+        mask = np.abs(PDimg.array) < thresh
+        PDimg.array[mask] = 0
+
+        PDFromImage = galsim.InterpolatedImage(PDimg)
+        P0PD = galsim.Convolve(P0FromImage, PDFromImage)
+        P0PDimg = P0PD.drawImage(nx=nx,ny=ny, scale=pixel_scale, method='no_pixel')
+        reconv_kernels.append(P0PDimg.array)
+        error = ((reconv_kernels[i]-kernels[i])**2).sum()
+        psf_error = max(psf_error, error)
+        #print fwhm, bands[i], error, nx*ny - mask.sum()
+    return kernels, diff_kernels, reconv_kernels, psf_error
+
+# get difference kernels for all PSFs
+import fitsio
+bands = ['g','r','i','z','y']
+objname = 'SDSSJ0231-0345'
+psf_bands = []
+for b in bands:
+    psf = galsim.fits.read("%s/psf-%s.fits" % (objname, b))
+    psf_bands.append(galsim.InterpolatedImage(psf))
+
+# reference kernel
+pixel_scale = 1.
+psf_thresh = 5e-3
+fwhms = np.arange(1., 3.2, 0.2)
+psf_errors = []
+for fwhm in fwhms:
+    P0 = galsim.Gaussian(fwhm=fwhm)
+    kernels, diff_kernels, reconv_kernels, psf_error = getDiffKernels(psf_bands, P0, thresh=psf_thresh)
+    psf_errors.append(psf_error)
+
+# get kernels with FWHM that has smallest pixelation errors
+min_psf = np.argmin(psf_errors)
+print "best overall PSF error: %r at FWHM = %.1f" % (psf_errors[min_psf], fwhms[min_psf])
+fwhm = fwhms[min_psf]
+P0 = galsim.Gaussian(fwhm=fwhm)
+#P0img = P0.drawImage(nx=nx,ny=ny, scale=pixel_scale, method='no_pixel')
+kernels, diff_kernels, reconv_kernels, psf_error = getDiffKernels(psf_bands, P0)
+
+# save difference kernels
+for i in range(len(bands)):
+    filename = "%s/psf-diff_kernel_%s.fits" % (objname, bands[i])
+    fitsio.write(filename, diff_kernels[i])
+
+# make a bands-combined coadds for detection
+os.system("mkdir multiband-coadds")
+imgs = []
+for b in bands:
+    hdu = fitsio.FITS("%s/stamp-%s.fits" % (objname, b))
+    imgs.append(hdu[0][:,:])
+sum_imgs = np.sum(imgs, axis=0)
+filename = "multiband-coadds/%s.fits" % objname
+fitsio.write(filename, sum_imgs)
