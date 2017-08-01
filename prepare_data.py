52a5ddfb3c66e82a838e00e25e8236a4bd6efdc1
Peter Melchior <peter.m.melchior@gmail.com>
2017-08-01 12:13:59 -0400

    fixed image transpose, now works on all directories

--- prepare_data.py
+++ prepare_data.py
@@ -1,6 +1,10 @@
 import numpy as np
 import galsim
 import os
+from sys import argv
+import fitsio
+
+bands = ['g','r','i','z','y']
 
 def getDiffKernel(P, P0, nx=50, ny=50):
     PD = galsim.Convolve(P, galsim.Deconvolve(P0))
@@ -15,7 +19,7 @@ def getDiffKernels(psf_bands, P0, thresh=5e-3):
 
     for i in range(len(psf_bands)):
         P = psf_bands[i]
-        nx,ny = P.image.array.shape
+        ny,nx = P.image.array.shape
 
         PDimg = getDiffKernel(P, P0, nx=nx, ny=ny)
         kernels.append(P.image.array)
@@ -36,51 +40,55 @@ def getDiffKernels(psf_bands, P0, thresh=5e-3):
         reconv_kernels.append(P0PDimg.array)
         error = ((reconv_kernels[i]-kernels[i])**2).sum()
         psf_error = max(psf_error, error)
-        #print fwhm, bands[i], error, nx*ny - mask.sum()
     return kernels, diff_kernels, reconv_kernels, psf_error
 
 # get difference kernels for all PSFs
-from sys import argv
-objname = argv[1]
 
-import fitsio
-bands = ['g','r','i','z','y']
-psf_bands = []
-for b in bands:
-    psf = galsim.fits.read("%s/psf-%s.fits" % (objname, b))
-    psf_bands.append(galsim.InterpolatedImage(psf))
+if len(argv) == 2:
+    objname = argv[1]
+    dirs = [objname]
+else:
+    from glob import glob
+    dirs = glob('SDSS*')
 
-# reference kernel
-pixel_scale = 1.
-psf_thresh = 5e-3
-fwhms = np.arange(1., 3.2, 0.2)
-psf_errors = []
-for fwhm in fwhms:
+for objname in dirs:
+    print objname,
+    psf_bands = []
+    for b in bands:
+        psf = galsim.fits.read("%s/psf-%s.fits" % (objname, b))
+        psf_bands.append(galsim.InterpolatedImage(psf))
+
+    # reference kernel
+    pixel_scale = 1.
+    psf_thresh = 5e-3
+    fwhms = np.arange(1., 3.2, 0.2)
+    psf_errors = []
+    for fwhm in fwhms:
+        P0 = galsim.Gaussian(fwhm=fwhm)
+        kernels, diff_kernels, reconv_kernels, psf_error = getDiffKernels(psf_bands, P0, thresh=psf_thresh)
+        psf_errors.append(psf_error)
+
+    # get kernels with FWHM that has smallest pixelation errors
+    min_psf = np.argmin(psf_errors)
+    print "best overall PSF error: %r at FWHM = %.1f" % (psf_errors[min_psf], fwhms[min_psf])
+    fwhm = fwhms[min_psf]
     P0 = galsim.Gaussian(fwhm=fwhm)
-    kernels, diff_kernels, reconv_kernels, psf_error = getDiffKernels(psf_bands, P0, thresh=psf_thresh)
-    psf_errors.append(psf_error)
+    #P0img = P0.drawImage(nx=nx,ny=ny, scale=pixel_scale, method='no_pixel')
+    kernels, diff_kernels, reconv_kernels, psf_error = getDiffKernels(psf_bands, P0)
 
-# get kernels with FWHM that has smallest pixelation errors
-min_psf = np.argmin(psf_errors)
-print "best overall PSF error: %r at FWHM = %.1f" % (psf_errors[min_psf], fwhms[min_psf])
-fwhm = fwhms[min_psf]
-P0 = galsim.Gaussian(fwhm=fwhm)
-#P0img = P0.drawImage(nx=nx,ny=ny, scale=pixel_scale, method='no_pixel')
-kernels, diff_kernels, reconv_kernels, psf_error = getDiffKernels(psf_bands, P0)
+    # save difference kernels
+    for i in range(len(bands)):
+        filename = "%s/psf-diff_kernel_%s.fits" % (objname, bands[i])
+        fitsio.write(filename, diff_kernels[i])
 
-# save difference kernels
-for i in range(len(bands)):
-    filename = "%s/psf-diff_kernel_%s.fits" % (objname, bands[i])
-    fitsio.write(filename, diff_kernels[i])
-
-"""
-# make a bands-combined coadds for detection
-os.system("mkdir multiband-coadds")
-imgs = []
-for b in bands:
-    hdu = fitsio.FITS("%s/stamp-%s.fits" % (objname, b))
-    imgs.append(hdu[0][:,:])
-sum_imgs = np.sum(imgs, axis=0)
-filename = "multiband-coadds/%s.fits" % objname
-fitsio.write(filename, sum_imgs)
-"""
+    """
+    # make a bands-combined coadds for detection
+    os.system("mkdir multiband-coadds")
+    imgs = []
+    for b in bands:
+        hdu = fitsio.FITS("%s/stamp-%s.fits" % (objname, b))
+        imgs.append(hdu[0][:,:])
+    sum_imgs = np.sum(imgs, axis=0)
+    filename = "multiband-coadds/%s.fits" % objname
+    fitsio.write(filename, sum_imgs)
+    """
